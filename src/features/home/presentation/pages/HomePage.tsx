e8a7947e1d70a3533c03898d3e5f61c1e8ee4575
Tharol Amar <tamar@altayer.com>
2026-06-09 12:53:25 +0530

    Initial clean architecture setup

---
+++ src/features/home/presentation/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1>❤️ Saii ❤️</h1>
+    </div>
+  );
+};
+
+export default HomePage;
